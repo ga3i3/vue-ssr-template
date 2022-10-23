625e2706b0a9f9fd3a303c0fe823556437cc83f7
Gaziz <gizbew@gmail.com>
2022-10-23 23:37:19 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app id="app">
+    <h1>{{title}}</h1>
+    <v-btn>123</v-btn>
+    <router-view></router-view>
+  </v-app>
+</template>
+
+<script>
+import { mapGetters } from 'vuex';
+
+export default {
+  name: 'App',
+
+  metaInfo: {
+    meta: [
+      { charset: 'utf-8' },
+    ]
+  },
+
+  computed: {
+    ...mapGetters({
+      title: 'title',
+    })
+  },
+};
+</script>
---
+++ src/modules/home/components/users-list/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul>
+        <li v-for="(user, index) in users" :key="index">{{user.name}} {{user.lastname}}</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: 'UsersList',
+        props: {
+            users: {
+                type: Array,
+                required: true
+            }
+        },
+    };
+</script>
